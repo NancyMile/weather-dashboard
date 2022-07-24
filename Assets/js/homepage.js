75057088af745045649f4b86734c0d2bf9baa262
nancy <smithnancy@outlook.com>
2022-07-24 12:03:57 +1000

    get daily weather function added now getting the data passing longitud and latitude to get the response in daily and the uvi

--- Assets/js/homepage.js
+++ Assets/js/homepage.js
@@ -3,6 +3,7 @@ var languageButtonsEl = document.querySelector('#language-buttons');
 var nameInputEl = document.querySelector('#cityname');
 var repoContainerEl = document.querySelector('#weatherparams-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+const myKey = '3647b99d321bbf401a1d1e6e104ff888';
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -30,10 +31,7 @@ var buttonClickHandler = function (event) {
 };
 
 var getCityWeather = function (city) {
- let myKey = '3647b99d321bbf401a1d1e6e104ff888';
-    //https://api.openweathermap.org/data/2.5/forecast5?q=Sydney&appid=3647b99d321bbf401a1d1e6e104ff888
-  var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&only_current=true&exclude=hourly&appid=${myKey}&cnt=5`;
-
+  var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&only_current=true&exclude=hourly&appid=${myKey}&cnt=1`;
   fetch(apiUrl)
     .then(function (response) {
       if (response.ok) {
@@ -44,84 +42,51 @@ var getCityWeather = function (city) {
         return response.json();
     })
         .then(function (data) {
-          console.log("aquiiii");  
           console.log(data);
-          displayWeather(data, city);
+          //if response was ok then  get the latitud and longitud for quering daily
+          getDailyWeather(data);
         })
-    //   } else {
-    //     alert('Error: ' + response.statusText);
-    //   }
-    //})
     .catch(function (error) {
       alert('Unable to connect'+error);
     });
 };
 
-// var getFeaturedRepos = function (language) {
-//   var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
-
-//   fetch(apiUrl).then(function (response) {
-//     if (response.ok) {
-//       response.json().then(function (data) {
-//         displayRepos(data.items, language);
-//       });
-//     } else {
-//       alert('Error: ' + response.statusText);
-//     }
-//   });
-// };
+var getDailyWeather = function (weatherResults) {
+    let lat = weatherResults.city.coord.lat;
+    let lon = weatherResults.city.coord.lon;
+    var apiUrlDaily = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=hourly&appid=${myKey}`;
+    fetch(apiUrlDaily).then(function (response) {
+        if (response.ok){
+            response.json().then(function (data) {
+            console.log("new response DAILY");
+            console.log(data);
+            displayWeather(data);
+        });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
+    });
+};
 
 var displayWeather = function (weatherParams, searchTerm) {
-    console.log('APPPPP');
-//   if (weatherParams.list.length === 0) {
-//     repoContainerEl.textContent = 'No data found.';
-//     return;
-//   }
-
-  repoSearchTerm.textContent = searchTerm;
-
-
-  repoContainerEl.innerHTML = weatherParams.list.map(day => {
-      //get the date from the dt timestamp
-      let dt = day.dt * 1000;
-      return `<section class="weather-card">
-      <header>${dt}</header>
-    <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="${day.weather[0].description}" />
-       <p>Temp: ${day.main.temp}</p>
-       <p>Wind: ${day.wind.speed}</p>
-       <p>Humidity: ${day.main.humdity}</p>
-       <p>UV Index: ??</p>
-    </section>`;
-  }).join(" ");
-
-
-  for (var i = 0; i < weatherParams.length; i++) {
-      console.log("ACAA");
-      console.log("LOOP "+weatherParams[i].weather[0].icon + '/' + weatherParams[i].wind.speed);
-    var repoName = weatherParams[i].weather[0].icon + '/' + weatherParams[i].wind.speed;
-
-    var repoEl = document.createElement('a');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
-    repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
-
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    if (weatherParams[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + weatherParams[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
+    repoSearchTerm.textContent = searchTerm;
+    // in roder to display five day forecast may have to reduce three out of eight days
+    for (var i = 0; i < weatherParams.daily.length-2; i++) {
+        // Parse the Unix timestamp and convert into any date format.
+        var weatherDate = moment.unix(weatherParams.daily[i].dt).format("MM/DD/YYYY");
+        //for each day create a card elemets  with the weather conditions
+        var weatherCard = document.createElement('section');
+        weatherCard.classList = "flex-row weather-card";
+        weatherCard.innerHTML = weatherCard.innerHTML +
+        `<section class="weather-card">
+            <header>${weatherDate}</header>
+            <img src="http://openweathermap.org/img/wn/${weatherParams.daily[i].weather[0].icon}@4x.png" alt="${weatherParams.daily[i].weather[0].description}" />
+            <p>Temp: ${weatherParams.daily[i].temp.day}</p>
+            <p>Wind: ${weatherParams.daily[i].wind_speed}</p>
+            <p>Humidity: ${weatherParams.daily[i].humdity}</p>
+            <p>UV Index: ${weatherParams.daily[i].uvi}</p>
+        </section>`;
+    repoContainerEl.appendChild(weatherCard);
   }
 };
 
